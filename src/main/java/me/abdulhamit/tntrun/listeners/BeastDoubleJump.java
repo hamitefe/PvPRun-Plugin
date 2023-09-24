a33c3d2edf2871f4d3a34fb4954f5d866cc204dd
hamit <hamitefe0161@gmail.com>
2023-09-24 11:18:54 +0300

    beast double jump fix (probably)

--- src/main/java/me/abdulhamit/tntrun/listeners/BeastDoubleJump.java
+++ src/main/java/me/abdulhamit/tntrun/listeners/BeastDoubleJump.java
@@ -1,10 +1,12 @@
 package me.abdulhamit.tntrun.listeners;
 
 import com.destroystokyo.paper.event.player.PlayerJumpEvent;
+import me.abdulhamit.tntrun.TNTRun;
 import me.abdulhamit.tntrun.game.Game;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerToggleFlightEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class BeastDoubleJump implements Listener {
     @EventHandler
@@ -12,8 +14,13 @@ public class BeastDoubleJump implements Listener {
         if (Game.current()==null)return;
         if (evt.getPlayer()!=Game.current().getBeast())return;
         
-        
-        evt.getPlayer().setFlying(false);
+        if (evt.isFlying()) {
+            new BukkitRunnable(){@Override
+            public void run(){
+                evt.getPlayer().setFlying(false);
+            }}.runTaskLater(TNTRun.getPlugin(TNTRun.class), 10);
+            
+        }
         evt.getPlayer().setVelocity(evt.getPlayer().getEyeLocation().getDirection().multiply(1));
         evt.getPlayer().setAllowFlight(false);
     }
